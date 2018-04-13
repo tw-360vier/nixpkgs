a231bfb793b058bad387780d978a97b753ed96b9
Thorsten Weber <tw@360vier.de>
2018-04-13 19:34:11 +0200

    Revert "gitlab: disable"
    This reverts commit 68c4605f1a251b97cb0fcde251ce52d498e2cef8.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -325,7 +325,7 @@
   ./services/misc/geoip-updater.nix
   ./services/misc/gitea.nix
   #./services/misc/gitit.nix
-  #./services/misc/gitlab.nix
+  ./services/misc/gitlab.nix
   ./services/misc/gitolite.nix
   ./services/misc/gitweb.nix
   ./services/misc/gogs.nix
